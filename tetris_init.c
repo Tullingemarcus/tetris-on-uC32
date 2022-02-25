071d3294315b60e85fe8f675dbc55acf689f8f6e
Tullingemarcus <marcus@tullingeskog.se>
2022-02-25 14:05:46 +0100

    add tetris initialization file

---
+++ tetris_init.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>      // Declarations of standard library
+#include <pic32mx.h>    // Declarations of system-specific addresses etc
+#include "tetris.h"     // Declarations for sepcific tetris function
+
+
+void init(){
+    TRISESET = 0x0ff;   // set bits 0-7 to output (0)
+    return;
+}
