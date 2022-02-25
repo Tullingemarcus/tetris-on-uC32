da6eb89be89cf2dcdec6c72191fd1e2d47c8ac56
Tullingemarcus <marcus@tullingeskog.se>
2022-02-25 15:25:24 +0100

    uppdate header to include all current functions

--- tetris.h
+++ tetris.h
@@ -1,7 +1,18 @@
 
-
+// declaration of function related to oled initialization
 void OledHostInit(void);
+unsigned char Spi2PutByte(unsigned char bVal);
+void OledDspInit(void);
+void OledUpdate(void);
+void OledPutBuffer(int cb, unsigned char *rgbTx);
+void display_led(void);
+
+// declaration of global constant and array
+#define cbOledDispMax   512 //max number of bytes in display buffer
+unsigned char rgbOledBmp[cbOledDispMax];
+
+// declaration of other functions
 void user_isr(void);
-void init();
+void init(void);
 void tetris(void);
 void delay(int);
