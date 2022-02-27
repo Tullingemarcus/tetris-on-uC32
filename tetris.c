a7769db0befdd1cf0ca7f560f100037c38847d8e
Tullingemarcus <marcus@tullingeskog.se>
2022-02-27 16:46:22 +0100

    uppdating tetris function to call for updatepixel function

--- tetris.c
+++ tetris.c
@@ -3,7 +3,7 @@
 #include <pic32mx.h>    // Declarations of system-specific addresses etc
 #include "tetris.h"     // Declarations for sepcific tetris function
 
-
+int blockVal = 0xe00e;
 void user_isr()
 {
     return;
@@ -13,6 +13,8 @@ void user_isr()
 // main function where the code will be run
 void tetris()
 {
-    OledUpdate();
+    blockPage(blockVal);
+    pixelmove();
+    delay(1000);
     return;
 }
