b94e3acda5d475d6800aec6e01a3314af8a79785
Tullingemarcus <marcus@tullingeskog.se>
2022-03-01 16:56:25 +0100

    updating tetris to handle multiple different blocks
    Co-authored-by: williamhedenskog <william.hedenskogg@gmail.com>

--- tetris.c
+++ tetris.c
@@ -1,34 +1,50 @@
-
 #include <stdio.h>      // Declarations of standard library
 #include <pic32mx.h>    // Declarations of system-specific addresses etc
 #include "tetris.h"     // Declarations for sepcific tetris function
 
-int blockVal = 0xfffe;
+int square = 0x0000001e;
+int stick = 0x000001fe;
+int pillar = 0x00000006;
+int delayVar = 20;
+int count = 0;
 
 void user_isr(void)
 {
     // PORTE += 1;
     if(IFS(0) & 0x8000){
-        blockVal = blockVal << 15;
-        PORTE += 1;
-        IFSCLR(0) = 0x08000;
-    }
-    if(IFS(0) & 0x0800){
-        blockVal = blockVal >> 15;
-        PORTE -= 1;
-        IFSCLR(0) = 0x00800;
+        while(!(PORTD & 0x0800)){
+
+        }
     }
     IFS(0) &= 0xfeff;
     
     return;
 }
 
+int getbtns(void){
+    int BTN2 =(PORTD & 0x020) >> 5;
+    int BTN3 =(PORTD & 0x040) >> 5;
+    int BTN4 =(PORTD & 0x080) >> 5;
+    int btnstate = BTN2 | BTN3 | BTN4;
+    return btnstate;
+}
 
 // main function where the code will be run
 void tetris()
 {
     delay(1000);
-    blockPage(blockVal);
-    pixelmove();
+    if(count == 1){
+        blockPage(pillar);
+        pillarUpdate();
+        count = 0;
+    }
+    else{
+        blockPage(square);
+        squareUpdate();
+        count = 1;
+    }
+    if(delayVar >= 10){
+        delayVar--;
+    }
     return;
 }
