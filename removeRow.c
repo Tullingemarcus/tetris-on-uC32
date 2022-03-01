94ed3170c357ea5004374b766010c2347fd3a1ba
Tullingemarcus <marcus@tullingeskog.se>
2022-03-01 16:54:56 +0100

    updating remove row with new remove limit
    Co-authored-by: williamhedenskog <william.hedenskogg@gmail.com>

--- removeRow.c
+++ removeRow.c
@@ -9,7 +9,7 @@ void removeRow(int k){
     if((rgbOledBmp[k] == 0xff) && (rgbOledBmp[k + 128] == 0xff) 
         && (rgbOledBmp[k + 256] == 0xff) && (rgbOledBmp[k + 384] == 0xff)){ 
         int j; 
-        for(j = k; j < 106; j++){
+        for(j = k; j < 101; j++){
             rgbOledBmp[j]       = rgbOledBmp[j + 4];  // remove row if full and shift down
             rgbOledBmp[j + 128] = rgbOledBmp[j + 4 + 128];
             rgbOledBmp[j + 256] = rgbOledBmp[j + 4 + 256];
