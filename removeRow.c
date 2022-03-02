e550ef7ad2dc32f6b1f49b74138f156082baf61c
Tullingemarcus <marcus@tullingeskog.se>
2022-03-02 18:15:06 +0100

    updating removerow to better handle different shapes
    Co-authored-by: williamhedenskog <william.hedenskogg@gmail.com>

--- removeRow.c
+++ removeRow.c
@@ -5,16 +5,17 @@
 
 
 void removeRow(int k){
-    //int i;
     if((rgbOledBmp[k] == 0xff) && (rgbOledBmp[k + 128] == 0xff) 
         && (rgbOledBmp[k + 256] == 0xff) && (rgbOledBmp[k + 384] == 0xff)){ 
         int j; 
         for(j = k; j < 101; j++){
-            rgbOledBmp[j]       = rgbOledBmp[j + 4];  // remove row if full and shift down
-            rgbOledBmp[j + 128] = rgbOledBmp[j + 4 + 128];
-            rgbOledBmp[j + 256] = rgbOledBmp[j + 4 + 256];
-            rgbOledBmp[j + 384] = rgbOledBmp[j + 4 + 384];            
+            rgbOledBmp[j]       = rgbOledBmp[j + 2];  // remove row if full and shift down
+            rgbOledBmp[j + 128] = rgbOledBmp[j + 2 + 128];
+            rgbOledBmp[j + 256] = rgbOledBmp[j + 2 + 256];
+            rgbOledBmp[j + 384] = rgbOledBmp[j + 2 + 384];
+                       
         }
+        OledUpdate(); 
         PORTE += 1;                     // increment LEDs by 1
     }
     return;
