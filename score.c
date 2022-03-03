175c3c652d68fe7d9d7642b3291c56dd933ab16c
Tullingemarcus <marcus@tullingeskog.se>
2022-03-03 13:19:53 +0100

    making score count points
    Co-authored-by: williamhedenskog <william.hedenskogg@gmail.com>

--- score.c
+++ score.c
@@ -3,42 +3,58 @@
 #include "tetris.h"     // Declarations for sepcific tetris function
 
 int points = 0;
+int points10 = 0;
+int points100 = 0;
+int points1000 = 0;
 void score(){
-    points += 0x0001;
-    int temp;
+    
+    points += 1;
+    if(points % 10 == 0){
+        points10 += 1;
+        points -= 10;
+        if(points10 % 10 == 0){
+            points100 += 1;
+            points10 -= 10;
+            if(points100 % 10 == 0){
+                points1000 += 1;
+                points100 -= 10;
+            }
+        }
+    }
+    int temp = (points) | (points10 << 4) | (points100 << 8) | (points1000 << 12);
     int i;
     for(i = 3; i > -1; i--){
-        temp = points - (10 * i);
         digitclr(i);
-        if(temp == 0){
+        if((temp & 0x000f) == 0){
             digit0(i);
         }
-        if(temp == 1){
+        if((temp & 0x000f) == 1){
             digit1(i);
         }
-        if(temp == 2){
+        if((temp & 0x000f) == 2){
             digit2(i);
         }
-        if(temp == 3){
+        if((temp & 0x000f) == 3){
             digit3(i);
         }
-        if(temp == 4){
+        if((temp & 0x000f) == 4){
             digit4(i);
         }
-        if(temp == 5){
+        if((temp & 0x000f) == 5){
             digit5(i);
         }
-        if(temp == 6){
+        if((temp & 0x000f) == 6){
             digit6(i);
         }
-        if(temp == 7){
+        if((temp & 0x000f) == 7){
             digit7(i);
         }
-        if(temp == 8){
+        if((temp & 0x000f) == 8){
             digit8(i);
         }
-        if(temp == 9){
+        if((temp & 0x000f) == 9){
             digit9(i);
         }
+        temp = temp >> 4;
     }  
 }
